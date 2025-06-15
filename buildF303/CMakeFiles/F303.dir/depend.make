c9af0e807a68a77fb5b77dac5b76c779716bafe2
Daniel Wegkamp <wegkamp@fhi-berlin.mpg.de>
2025-06-15 13:24:52 +0100

    ...

--- buildF303/CMakeFiles/F303.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for F303.
-# This may be replaced when dependencies are built.
--- main.c
+++ main.c
@@ -14,41 +14,38 @@ uint32_t *isr_vectors[] =
 	(uint32_t *)Default_Handler,
 };
 
-//add adc interrupt?
-
-
 #define PIN 5
-
-
-
-
-//uint32_t someGlobal;
-//direct memory access: *(volatile uint32_t *)(some address) = some value
 
 int main( void )
 {
 
-	GPIOinit();
-	GPIOchangeFunction( PIN, PIN_OUTPUT );
+	GPIO_init();
+	GPIO_changeFunction( PIN, PIN_OUTPUT );
 
 
 	//test debugging capabilities by just writing to RAM: (seems to work fine)
 	uint32_t loop_counter = 0x10000000;
-	setWord( 0x20000200, loop_counter++ );
+	setWord( 0x20001800, loop_counter++ );
+
+/*	for (int i = 0; i < 100; i++)
+	{
+		__asm("nop");
+	}*/
+
 
 	STMtest();
 
 	while(1)
 	{
 		//set diode:
-		GPIOset( PIN );
-		expensive_wait( 2 );
-
-		//unset diode
-		GPIOunset( PIN );
+		GPIO_set( PIN );
 		expensive_wait( 1 );
 
-		setWord( 0x20000200, loop_counter++ );
+		//unset diode
+		GPIO_unset( PIN );
+		expensive_wait( 3 );
+
+		setWord( 0x20001800, loop_counter++ );
 	}
 
 	return 0;
@@ -68,7 +65,7 @@ void Default_Handler( void )
 void expensive_wait( int multiplier )
 {
 	volatile int i = 0;
-	multiplier *= 200000;
+	multiplier *= 20000;
 
 	for( i = 0; i < multiplier; i++ )
 	{
